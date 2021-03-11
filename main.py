a64fcb3facb119aa421f57b99729ae98f1f5d91b
Jake Garth <jake.garth@students.mq.edu.au>
2021-03-11 16:28:14 +1100

    Traceroute and hops now implemented

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import requests
 import urllib.request
 from website_functions import *
 import pydnsbl
+import csv
 
 
-def getResponseCodeList():
+def writeToCSVMethod(mylist, fileName):
+    with open(fileName, 'a', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(mylist)
+
+def WriteResultsList(domainList, writeFile):
     websiteList = []
-    with open("100MostVisitedSites.txt") as fp:
+    with open(domainList) as fp:
         Lines = fp.readlines()
     for line in Lines:
         websiteList.append(line.strip('\n'))
@@ -14,12 +20,15 @@ def getResponseCodeList():
 
     ourIP = str(getIPAddress())
 
-    AARNFile =  open("Most_Visited.txt","w", encoding="utf-8")
+    #AARNFile =  open("Most_Visited.txt","w", encoding="utf-8")
     for item in websiteList:
         positionofWWW = item.find('://')
 
+        if "http" in item:
+            WebsiteNOHttp = item[positionofWWW+3:]
+        else:
         #If http in domain name, change to + 3, if no http, change to +1
-        WebsiteNOHttp = item[positionofWWW+1:]
+            WebsiteNOHttp = item[positionofWWW+1:]
         print(WebsiteNOHttp)
         try:
 
@@ -41,7 +50,27 @@ def getResponseCodeList():
             IP = str(e)
 
         responseCODE = responseCODE.replace(',',';')
-        AARNFile.write(item + "," + str(responseCODE) +"," +IP + "\n")
+
+
+        print(WebsiteNOHttp[0:4])
+        print(WebsiteNOHttp)
+        if 'www.' == WebsiteNOHttp[0:4]:
+            print("change")
+            WebsiteNoWWWNoSlash = WebsiteNOHttp[4:]
+        else:
+            WebsiteNoWWWNoSlash = WebsiteNOHttp
+        if '/' == WebsiteNoWWWNoSlash[-1]:
+            WebsiteNoWWWNoSlash = WebsiteNoWWWNoSlash[0:-1]
+
+        print(WebsiteNoWWWNoSlash)
+        hopList = scapyTracerouteWithSR(WebsiteNoWWWNoSlash)
+        hopNumber = len(hopList)
+        hopListSting = str(hopList).replace(',',';')
+
+        resultsList = [item, responseCODE, IP, hopNumber, hopListSting]
+
+        writeToCSVMethod(resultsList, writeFile)
+        #AARNFile.write(item + "," + str(responseCODE) +"," +IP + "\n")
 
     AARNFile.close()
 
@@ -49,7 +78,44 @@ def getResponseCodeList():
 
 
 def main():
-    getResponseCodeList()
+
+    #print(scapyTracerouteWithSR('cisco.com'))
+    openFile = "CopyRight_Telstra.txt"
+    writeFile = "Copy_Right_Telstra_Results_UNI.csv"
+    WriteResultsList(openFile, writeFile)
+
+    #print(CompareDNSResults("facebook.com"))
+    #print((getTraceRouteList("facebook.com")))
+
+
+
+
+
+    #CompareDNSResults("cisco.com")
+
+
+
+    '''
+    print(len(getTraceRouteList("cisco.com")))
+    print(getIPAddressOfDomain("cisco.com"))
+
+
+
+    dns_resolver = dns.resolver.Resolver()
+    dns_resolver.nameservers[0]
+    print(type(dns_resolver.nameservers[0]))
+    print(dns_resolver.nameservers[0])
+    print("DNS Traceroute Google DNS:")
+    print(str(DNSTraceroute('8.8.8.8')[0]))
+
+    '''
+
+    #print("DNS Traceroute Default DNS:")
+    #print(str(DNSTraceroute(dns_resolver.nameservers[0])[0]))
+
+
+    #print(str(DNSTraceroute(dns_resolver.nameservers[0])[1]))
+    #WriteResultsList()
 
 
 
--- website_functions.py
+++ website_functions.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import socket
+from scapy.all import *
+import dns.resolver
 
 
 
@@ -61,3 +63,88 @@ def getIPAddress():
     hostname = socket.gethostname()
     IPAddr = socket.gethostbyname(hostname)
     return IPAddr
+
+
+def CompareDNSResults(website): #this may be legacy...might use the change DNS in the future
+    dns_resolver = dns.resolver.Resolver()
+    DNSList = [dns_resolver.nameservers[0]]
+    #DNSList = [dns_resolver.nameservers[0],'8.8.8.8','1.1.1.1'] #cloudflare and google's DNS
+
+    for DNS_Address in DNSList:
+        print(DNS)
+
+        ans, unans = traceroute(DNS_Address,l4=UDP(sport=RandShort())/DNS(qd=DNSQR(qname=website)),maxttl=15)
+        #ans, unans = traceroute(DNS,l4=UDP(sport=RandShort())/DNS(qd=DNSQR(qname='google.com')),maxttl=15)
+        print("Number of hops:")
+        print(ans[TCP])
+        for snd, _ in ans[TCP]:
+            print(type(snd))
+            print(snd, _)
+            print(type(snd[IP]))
+            print(snd[IP])
+            print(type(snd[IP].ttl))
+            print(snd[IP].ttl)
+            print("what")
+
+        #print((ans[TCP]))
+        #print(type(ans))
+        #print(ans)
+        #print(type(unans))
+        #print(unans)
+        #for snd, rcv in ans:
+        #    print(snd.ttl, rcv.src, snd.sent_time, rcv.time)
+
+
+
+def DNSTraceroute(DNSServerAddress):
+
+    ans, unans = traceroute(DNSServerAddress,l4=UDP(sport=RandShort())/DNS(qd=DNSQR(qname="cisco.com")),maxttl=15)
+    ans.graph()
+    return ans, unans
+
+def getTraceRouteList(host):
+
+    print("Traceroute", host)
+    flag = True
+    ttl=1
+    hops = []
+    while flag:
+        ans, unans = sr(IP(dst=host,ttl=ttl)/ICMP(), timeout = 10)
+        try:
+            gotdata = ans.res[0][1]
+        except IndexError:
+            gotdata = 'null'
+            hops = ['Error in Traceroute']
+            return hops
+
+        if ans.res[0][1].type == 0: # checking for  ICMP echo-reply
+            flag = False
+        else:
+            hops.append(ans.res[0][1].src) # storing the src ip from ICMP error message
+            ttl +=1
+        #print("ans.res:")
+        #print(ans.res) #Use this to  see the src and destination of each request, this lets u see the hops working in more details
+    i = 1
+    for hop in hops:
+        print(i, " " + hop)
+        i+=1
+    return hops
+
+
+def scapyTracerouteWithSR(domain):
+    try:
+        ans, unans = sr(IP(dst=domain, ttl=(1,25),id=RandShort())/TCP(flags=0x2), timeout = 2)
+    except Exception as e:
+        return [str(e)]
+    hops = []
+    for snd,rcv in ans:
+        print(snd.ttl, rcv.src, isinstance(rcv.payload, TCP))
+
+        if len(hops) > 0:
+            if not isinstance(rcv.payload, TCP) or hops[-1] != rcv.src:
+                hops.append(rcv.src)
+        else:
+            if not isinstance(rcv.payload, TCP):
+                hops.append(rcv.src)
+
+    return hops
